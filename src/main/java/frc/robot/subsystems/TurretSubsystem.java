6fb7439ea3555daf265181d2625237b7210319de
Owen Rossing <me6apl1er@gmail.com>
2026-01-26 17:40:28 -0600

    End of day

--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import static com.ctre.phoenix6.signals.NeutralModeValue.Brake;
+import static frc.robot.Constants.TurretConstants.YAW_STATOR_CURRENT_LIMIT;
+import static frc.robot.Constants.TurretConstants.YAW_SUPPLY_CURRENT_LIMIT;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
@@ -12,14 +14,14 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 @Logged(strategy = Strategy.OPT_IN)
 public class TurretSubsystem extends SubsystemBase {
 
-  private final TalonFX yawMotor = new TalonFX(10); // TODO: Verify ID and naming scheme
-  // TODO: Add encoder type
+  private final TalonFX yawMotor = new TalonFX(25); // TODO: Verify naming scheme
+  // TODO: 10 turn pot (analog 0)
 
-  private final TalonFX hoodMotor = new TalonFX(12); // TODO: Verify ID and naming scheme
-  // TODO: Add encoder type
+  private final TalonFX pitchMotor = new TalonFX(26); // TODO: Verify naming scheme
+  // TODO: Verify encoder type and ID (heard CanCoder and in motor encoder)
 
   private final MotionMagicVoltage yawControl = new MotionMagicVoltage(0.0).withEnableFOC(true);
-  private final MotionMagicVoltage hoodControl = new MotionMagicVoltage(0.0).withEnableFOC(true);
+  private final MotionMagicVoltage pitchControl = new MotionMagicVoltage(0.0).withEnableFOC(true);
 
   // TODO: SysIdRoutine for yaw and hood motors
 
@@ -28,6 +30,10 @@ public class TurretSubsystem extends SubsystemBase {
 
     var yawTalonConfig = new TalonFXConfiguration();
     yawTalonConfig.MotorOutput.NeutralMode = Brake;
+    yawTalonConfig.CurrentLimits.StatorCurrentLimit = YAW_STATOR_CURRENT_LIMIT; // TODO: .in(Amps);
+    yawTalonConfig.CurrentLimits.StatorCurrentLimitEnable = true;
+    yawTalonConfig.CurrentLimits.SupplyCurrentLimit = YAW_SUPPLY_CURRENT_LIMIT; // TODO: .in(Amps);
+    yawTalonConfig.CurrentLimits.SupplyCurrentLimitEnable = true;
   }
 
   @Override
