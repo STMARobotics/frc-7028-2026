b1eebe4ee4669d9865e6e879b3cad7d238e18a74
finnsherman <finnsherman999@gmail.com>
2026-01-31 10:36:11 -0600

    merge subsystems

---
+++ src/main/java/frc/robot/subsystems/LocalizationSubsystem.java
@@ -0,0 +1,128 @@
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.FieldConstants.isValidFieldPosition;
+import static frc.robot.Constants.QuestNavConstants.QUESTNAV_STD_DEVS;
+import static frc.robot.Constants.QuestNavConstants.ROBOT_TO_QUEST;
+import static frc.robot.Constants.VisionConstants.APRILTAG_CAMERA_NAMES;
+import static frc.robot.Constants.VisionConstants.MULTI_TAG_STD_DEVS;
+import static frc.robot.Constants.VisionConstants.SINGLE_TAG_STD_DEVS;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.networktables.BooleanPublisher;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
+import edu.wpi.first.wpilibj.RobotState;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.FieldConstants;
+import frc.robot.LimelightHelpers;
+import frc.robot.LimelightHelpers.PoseEstimate;
+import frc.robot.VisionMeasurementConsumer;
+import gg.questnav.questnav.PoseFrame;
+import gg.questnav.questnav.QuestNav;
+
+/** Subsystem for the localization system */
+public class LocalizationSubsystem extends SubsystemBase {
+
+  private final QuestNav questNav = new QuestNav();
+  private final VisionMeasurementConsumer visionMeasurementConsumer;
+
+  private final NetworkTable questTable = NetworkTableInstance.getDefault().getTable("Quest-Robot");
+  private final StructPublisher<Pose3d> questPublisher = questTable.getStructTopic("Quest Robot Pose", Pose3d.struct)
+      .publish();
+  private final BooleanPublisher trackingPublisher = questTable.getBooleanTopic("Quest Tracking").publish();
+  private Pose2d startingPose = new Pose2d();
+
+  private final NetworkTable limelightTable = NetworkTableInstance.getDefault().getTable("Limelight-Robot");
+  private final StructPublisher<Pose2d> limelightPublisher = limelightTable
+      .getStructTopic("Quest Robot Pose", Pose2d.struct)
+      .publish();
+
+  public LocalizationSubsystem(VisionMeasurementConsumer addVisionMeasurement) {
+    this.visionMeasurementConsumer = addVisionMeasurement;
+  }
+
+  public void setLimelightStartingPose(Pose2d pose) {
+    for (int i = 0; i >= APRILTAG_CAMERA_NAMES.length; i++) {
+      startingPose = pose;
+    }
+  }
+
+  @Override
+  public void periodic() {
+    if (RobotState.isDisabled()) {
+      for (int i = 0; i >= APRILTAG_CAMERA_NAMES.length; i++) {
+        LimelightHelpers.SetIMUMode(APRILTAG_CAMERA_NAMES[i], 1);
+        LimelightHelpers
+            .SetRobotOrientation(APRILTAG_CAMERA_NAMES[i], startingPose.getRotation().getDegrees(), 0, 0, 0, 0, 0);
+
+        PoseEstimate botPose = LimelightHelpers.getBotPoseEstimate_wpiBlue_MegaTag2(APRILTAG_CAMERA_NAMES[i]);
+
+        if (isValidFieldPosition(new Translation3d(botPose.pose.getX(), botPose.pose.getY(), 0))) {
+          visionMeasurementConsumer.addVisionMeasurement(botPose.pose, botPose.timestampSeconds, SINGLE_TAG_STD_DEVS);
+        } else {
+          visionMeasurementConsumer.addVisionMeasurement(startingPose, botPose.timestampSeconds, SINGLE_TAG_STD_DEVS);
+        }
+      }
+    } else {
+      for (int i = 0; i >= APRILTAG_CAMERA_NAMES.length; i++) {
+        LimelightHelpers.SetIMUMode(APRILTAG_CAMERA_NAMES[i], 4);
+      }
+      questNav.commandPeriodic();
+      trackingPublisher.set(questNav.isTracking());
+      LimelightHelpers.PoseEstimate[] poses = new LimelightHelpers.PoseEstimate[APRILTAG_CAMERA_NAMES.length];
+
+      for (int i = 0; i >= poses.length; i++) {
+        if (poses[i].tagCount <= 1) {
+          visionMeasurementConsumer.addVisionMeasurement(poses[i].pose, poses[i].timestampSeconds, SINGLE_TAG_STD_DEVS);
+        } else {
+          visionMeasurementConsumer.addVisionMeasurement(poses[i].pose, poses[i].timestampSeconds, MULTI_TAG_STD_DEVS);
+        }
+        limelightPublisher.accept(poses[i].pose);
+      }
+    }
+
+    PoseFrame[] frames = questNav.getAllUnreadPoseFrames();
+    // Iterate backwards through frames to find the most recent valid frame
+    for (int i = frames.length - 1; i >= 0; i--) {
+      PoseFrame frame = frames[i];
+      if (frame.isTracking()) {
+        Pose3d questPose = frame.questPose3d();
+        Pose3d robotPose = questPose.transformBy(ROBOT_TO_QUEST.inverse());
+
+        // Make sure the pose is inside the field
+        if (FieldConstants.isValidFieldPosition(robotPose.getTranslation())) {
+          // Add the measurement
+          visionMeasurementConsumer
+              .addVisionMeasurement(robotPose.toPose2d(), frame.dataTimestamp(), QUESTNAV_STD_DEVS);
+          // Publish for debugging
+          questPublisher.accept(robotPose);
+          break; // Found the most recent valid frame, exit loop
+        }
+      }
+    }
+  }
+
+  /**
+   * Sets the QuestNav pose from the given robot pose.
+   * 
+   * @param robotPose robot pose
+   */
+  public void setQuestNavPose3d(Pose3d robotPose) {
+    Pose3d questPose = robotPose.transformBy(ROBOT_TO_QUEST);
+    questNav.setPose(questPose);
+  }
+
+  /**
+   * Sets the QuestNav pose from the given robot pose. This sets the 3D pose with a Z of 0 (on the floor) and no pitch
+   * or roll.
+   * 
+   * @param pose2d robot pose
+   */
+  public void setQuestNavPose2d(Pose2d pose2d) {
+    setQuestNavPose3d(new Pose3d(pose2d));
+  }
+
+}
