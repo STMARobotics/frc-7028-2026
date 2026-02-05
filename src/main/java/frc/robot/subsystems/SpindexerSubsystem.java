d5f4ffd313851b537996dabc63ada64c0b290ff3
mollie <158850656+molliemur@users.noreply.github.com>
2026-02-04 18:34:46 -0600

    Add hopper camera to detect full and empty hopper

--- src/main/java/frc/robot/subsystems/SpindexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/SpindexerSubsystem.java
@@ -11,20 +11,35 @@ import com.ctre.phoenix6.controls.VelocityTorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.Constants;
+import java.util.List;
+import org.photonvision.*;
+import org.photonvision.targeting.PhotonPipelineResult;
+import org.photonvision.targeting.PhotonTrackedTarget;
 
 /**
  * The is the Subsystem for the Spindexer.
+ * 
  */
-public class SpindexerSubsystem extends SubsystemBase {
 
+public class SpindexerSubsystem extends SubsystemBase {
   private final TalonFX spindexerMotor = new TalonFX(Constants.SpindexerConstants.DEVICE_ID_SPINDEXER_MOTOR);
   private final VelocityTorqueCurrentFOC spindexerVelocityTorque = new VelocityTorqueCurrentFOC(0.0);
   private final TorqueCurrentFOC spindexerTorqueControl = new TorqueCurrentFOC(0.0);
+  private final PhotonCamera hopperCam = new PhotonCamera(Constants.SpindexerConstants.HOPPER_CAMERA_NAME);
+  private static final double PIPELINE_RESULT_TTL = 0.25;
+  private static final PhotonPipelineResult EMPTY_PHOTON_PIPELINE_RESULT = new PhotonPipelineResult();
+  private PhotonPipelineResult photonPipelineResult = EMPTY_PHOTON_PIPELINE_RESULT;
+
+  private enum SpindexerDirection {
+    Forward,
+    Backward
+  }
 
   // NOTE: the output type is amps, NOT volts (even though it says volts)
   // https://www.chiefdelphi.com/t/sysid-with-ctre-swerve-characterization/452631/8
@@ -66,7 +81,7 @@ public class SpindexerSubsystem extends SubsystemBase {
   }
 
   // Spins the spindexer forward to feed the shooter
-  public void feedTransfer() {
+  public void feedShooter() {
     spindexerMotor
         .setControl(spindexerVelocityTorque.withVelocity(Constants.SpindexerConstants.SPINDEXER_FEED_VELOCITY));
   }
@@ -77,14 +92,65 @@ public class SpindexerSubsystem extends SubsystemBase {
         .setControl(spindexerVelocityTorque.withVelocity(Constants.SpindexerConstants.SPINDEXER_INTAKE_VELOCITY));
   }
 
-  // Agitates the spindexer to prevent jams
-  public void agitate() {
-    spindexerMotor
-        .setControl(spindexerVelocityTorque.withVelocity(Constants.SpindexerConstants.SPINDEXER_AGITATE_VELOCITY));
+  // Agitates the spindexer back and forth to prevent jams
+  public Command agitate() {
+    return run(() -> this.spin(SpindexerDirection.Forward)).withTimeout(0.5)
+        .andThen(run(() -> this.spin(SpindexerDirection.Backward)).withTimeout(0.5))
+        .repeatedly()
+        .finallyDo(this::stop);
   }
 
   // Stops the spindexer
   public void stop() {
     spindexerMotor.stopMotor();
+  }
+
+  public boolean isEmpty() {
+    return getLatestTarget().isEmpty();
+  }
+
+  public boolean isFull() {
+    if (isEmpty()) {
+      return false;
+    }
+    double accumulatedArea = 0;
+    for (var target : getLatestTarget()) {
+      accumulatedArea += target.getArea();
+      if (accumulatedArea >= Constants.SpindexerConstants.HOPPER_FULL_THRESHOLD) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private void spin(SpindexerDirection direction) {
+    if (direction == SpindexerDirection.Backward) {
+      spindexerMotor.setControl(
+          spindexerVelocityTorque.withVelocity(Constants.SpindexerConstants.SPINDEXER_AGITATE_BACKWARDS_VELOCITY));
+    } else {
+      spindexerMotor.setControl(
+          spindexerVelocityTorque.withVelocity(Constants.SpindexerConstants.SPINDEXER_AGITATE_FORWARDS_VELOCITY));
+    }
+  }
+
+  private List<PhotonTrackedTarget> getLatestTarget() {
+    List<PhotonPipelineResult> photonResults = hopperCam.getAllUnreadResults();
+
+    // If there are results this will grab the last item on the list
+    if (photonResults.size() >= 1) {
+      photonPipelineResult = photonResults.get(photonResults.size() - 1);
+    }
+
+    double currentTime = Timer.getFPGATimestamp();
+    double resultTime = photonPipelineResult.getTimestampSeconds();
+
+    // since the results are cached, the value is checked against the Time To Live (TTL) to
+    // ensure that the value isn't to old
+    if (currentTime - resultTime >= PIPELINE_RESULT_TTL) {
+      photonPipelineResult = EMPTY_PHOTON_PIPELINE_RESULT;
+    }
+
+    return photonPipelineResult.targets;
   }
 }
