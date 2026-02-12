88721b2ee6a71da2af233dfb8660fe703dfa771c
math-rad <ben.sir545@outlook.com>
2026-02-12 17:20:48 -0600

    crappy jacobian, thats not optimized and could probably be done in one loop

--- physics-sim/src/main/java/com/frc7028/physics/sim/AdaptiveErrorCorrectionSystem.java
+++ physics-sim/src/main/java/com/frc7028/physics/sim/AdaptiveErrorCorrectionSystem.java
@@ -3,42 +3,59 @@ package com.frc7028.physics.sim;
 import java.util.function.Function;
 import org.ejml.simple.SimpleMatrix;
 
-public class AdaptiveErrorCorrectionSystem<inputState, N, M> {
+public class AdaptiveErrorCorrectionSystem {
 
-  Function<inputState, SimpleMatrix> getInputs;
-  Function<SimpleMatrix, SimpleMatrix> computeSystem;
+  Function<SimpleMatrix, SimpleMatrix> computeError;
   SimpleMatrix inputDeltas;
+  SimpleMatrix deltaMatrix;
+  // error(n) x input(m)
+  // [ ]
+  // [ ]
+  // [ ]
 
   int N;
   int M;
 
   public AdaptiveErrorCorrectionSystem(
-      Function<inputState, SimpleMatrix> getInputs,
-      Function<SimpleMatrix, SimpleMatrix> computeSystem,
+      Function<SimpleMatrix, SimpleMatrix> computeErrorFunction,
       SimpleMatrix inputDeltas,
       int N,
       int M) {
-    this.getInputs = getInputs;
-    this.computeSystem = computeSystem;
+
+    this.computeError = computeErrorFunction;
+    this.N = N;
+    this.M = M;
+    this.deltaMatrix = new SimpleMatrix(M, M);
+    for (int inputIndex = 0; inputIndex < M; inputIndex++) {
+      this.deltaMatrix.set(inputIndex, inputIndex, inputDeltas.get(inputIndex, 0));
+    }
   }
 
-  public SimpleMatrix computeCorrection(inputState input) {
-    SimpleMatrix baselineInputs = this.getInputs.apply(input);
-    SimpleMatrix baselineErrors = this.computeSystem.apply(baselineInputs);
+  public SimpleMatrix computeCorrection(SimpleMatrix baselineInputs) {
+    SimpleMatrix baselineErrors = this.computeError.apply(baselineInputs);
 
     SimpleMatrix inputMatrix = new SimpleMatrix(this.M, this.M);
     SimpleMatrix outputMatrix = new SimpleMatrix(this.N, this.M);
-    SimpleMatrix differenceMatrix = new SimpleMatrix(this.N, this.M);
-    SimpleMatrix jacobian = new SimpleMatrix(this.N, this.M);
 
-    for (int inputIndex = 0; inputIndex < this.N; inputIndex++) {
-      inputMatrix.setRow(inputIndex, baselineErrors);
-      inputMatrix.set(inputIndex, inputIndex, baselineErrors.get(inputIndex) + this.inputDeltas.get(inputIndex));
-      outputMatrix.setRow(inputIndex, this.computeSystem.apply(inputMatrix.getRow(inputIndex)));
-      differenceMatrix.setRow(inputIndex, baselineErrors.minus(outputMatrix.getRow(inputIndex)));
-      jacobian.setRow(inputIndex, differenceMatrix.getRow(inputIndex).elementDiv(this.inputDeltas));
+    SimpleMatrix baselineReferenceMatrix = new SimpleMatrix(this.N, this.M);
+
+    for (int inputIndex = 0; inputIndex < this.M; inputIndex++) {
+      inputMatrix.setRow(inputIndex, baselineInputs.transpose());
+      baselineReferenceMatrix.setColumn(inputIndex, baselineErrors);
     }
 
-    return jacobian.pseudoInverse().mult(baselineErrors);
+    SimpleMatrix perturbedInputMatrix = inputMatrix.plus(this.deltaMatrix);
+
+    for (int inputIndex = 0; inputIndex < this.M; inputIndex++) {
+      outputMatrix.setColumn(inputIndex, this.computeError.apply(perturbedInputMatrix.getRow(inputIndex).transpose()));
+    }
+
+    outputMatrix = outputMatrix.minus(baselineReferenceMatrix);
+
+    for (int inputIndex = 0; inputIndex < this.M; inputIndex++) {
+      outputMatrix.setColumn(inputIndex, outputMatrix.getColumn(inputIndex).divide(inputDeltas.get(inputIndex, 0)));
+    }
+
+    return outputMatrix.pseudoInverse().mult(baselineErrors);
   }
 }
