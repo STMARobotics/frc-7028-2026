4b68ceca292c4e331b28c19771951b3b7543f970
finnsherman <finnsherman999@gmail.com>
2026-02-13 17:27:59 -0600

    Fixed the problems in my pr

--- src/main/java/frc/robot/commands/ShootCommand.java
+++ src/main/java/frc/robot/commands/ShootCommand.java
@@ -1,9 +1,16 @@
 package frc.robot.commands;
 
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants;
 import frc.robot.subsystems.ShooterSubsystem;
 import frc.robot.subsystems.SpindexerSubsystem;
 import frc.robot.subsystems.TransferSubsystem;
+
+/*
+ * Command to shoot balls from the Spindexer
+ */
 
 public class ShootCommand extends Command {
   private final SpindexerSubsystem spindexerSubsystem;
@@ -24,8 +31,17 @@ public class ShootCommand extends Command {
   @Override
   public void execute() {
     shooterSubsystem.isReadyToShoot();
-    shooterSubsystem.applySetpoints(null);
-    if (shooterSubsystem.isFlywheelAtSpeed()) {
+    /*
+     * sets the Yaw, Pitch, and Angle
+     */
+    shooterSubsystem.setYawAngle(Constants.ShooterConstants.YAW_HOME_ANGLE);
+    shooterSubsystem.setPitchAngle(Constants.ShooterConstants.PITCH_HOME_ANGLE);
+    shooterSubsystem.setFlywheelSpeed(RotationsPerSecond.of(40));
+    /*
+     * Checks to make sure the shooter is ready and up to speed
+     * before runnig the spindexer and transfer
+     */
+    if (shooterSubsystem.isReadyToShoot()) {
       spindexerSubsystem.feedShooter();
       transferSubsystem.feedShooter();
     }
